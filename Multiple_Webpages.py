811ec090db526d75aef35c4f26dc7bc888ece453
TejaAmbati-02 <73156927+TejaAmbati-02@users.noreply.github.com>
2022-05-05 19:25:37 +0530

    Add files via upload

---
+++ Multiple_Webpages.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from axe_selenium_python import Axe
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+import xlrd
+import shutil
+import os
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+loc = ("aaa2797.xlsx")
+files = os.listdir("AxeAutomationTool")
+
+wb = xlrd.open_workbook(loc)
+sheet = wb.sheet_by_index(0)
+sheet.cell_value(0,0)
+
+lis = []
+for i in range(0, sheet.nrows):
+	a = sheet.cell_value(i, 0)
+	lis.append(a)
+
+def test_google():
+	count = 1
+	for i in lis:
+		driver.get(i)
+		axe = Axe(driver)
+		axe.inject()
+		results = axe.run()
+		axe.write_results(results, f'axeIntegration{count}{i[12:16]}.json')
+		count+=1
+
+	for f in files:
+		if f.endswith(".json"):
+			shutil.move(f, "JSONOutputFiles")
+	driver.close()
+	assert len(results["violations"]) == 0, axe.report(results["violations"])
+
+
+
+
+
+test_google()
